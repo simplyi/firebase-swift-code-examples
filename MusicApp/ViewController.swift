f6880ba0b77369065d71a1fcdeadbe47bbc36579
Sergey Kargopolov <skargopolov@central1.com>
2018-01-16 06:04:05 -0800

    Implemented User Sign up with Firebase

--- MusicApp/ViewController.swift
+++ MusicApp/ViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +28,77 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func signupButtonTapped(_ sender: Any) {
+     guard let firstName = firstNameTextField.text, !firstName.isEmpty else {
+            self.showMessage(messageToDisplay: "First name is required!")
+            return;
+        }
+     guard let lastName = lastNameTextField.text, !lastName.isEmpty else {
+            self.showMessage(messageToDisplay: "Last name is required!")
+            return;
+        }
+    guard let userEmailAddress = emailAddressTextField.text, !userEmailAddress.isEmpty else {
+            self.showMessage(messageToDisplay: "Email is required!")
+            return;
+        }
+        
+    guard let userPassword = passwordTextField.text, !userPassword.isEmpty else {
+            self.showMessage(messageToDisplay: "User password is required!")
+            return;
+        }
+    guard let userRepeatPassword = repeatPassword.text, !userRepeatPassword.isEmpty else {
+            self.showMessage(messageToDisplay: "User repeat password is required!")
+            return;
+        }
+        
+        
+        if userPassword != userRepeatPassword
+        {
+            self.showMessage(messageToDisplay: "User provided passwords do not match")
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: userEmailAddress, password: userPassword) { (user, error) in
+            
+            if let error = error
+            {
+                print(error.localizedDescription)
+                self.showMessage(messageToDisplay: error.localizedDescription)
+                return
+            }
+            
+            if let user = user {
+                var databaseReference: DatabaseReference!
+                databaseReference = Database.database().reference()
+                
+                let userDetails:[String:String] = ["firstName":firstName, "lastName":lastName]
+                
+         databaseReference.child("users").child(user.uid).setValue(["userDetails":userDetails])
+                
+                
+            }
+            
+        }
+     
+        
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+    }
+    
+    public func showMessage(messageToDisplay:String)
+    {
+        let alertController = UIAlertController(title: "Alert title", message: messageToDisplay, preferredStyle: .alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
+            // Code in this block will trigger when OK button tapped.
+            print("Ok button tapped");
+            }
+        
+        alertController.addAction(OKAction)
+        
+        self.present(alertController, animated: true, completion:nil)
+    }
+ 
 }
 
---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'MusicApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MusicApp
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+
+  target 'MusicAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'MusicAppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
